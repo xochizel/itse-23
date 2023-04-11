0274256feba6a7ce1c4b90dfb42f411dea97c28c
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-11 18:33:38 -0500

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var name = "Xochitl"
+    var age = 18
+    var annualPay:Double
+    annualPay = 50000.00
+    print("My name is $name, my age is $age, and I want to make $ $annualPay per year")
+
+
+
+}
