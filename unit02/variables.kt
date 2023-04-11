e45bb8c90b9674d0f2e58ec46d8367cdc0258faf
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-11 17:04:01 -0500

    Update variables.kt

--- unit02/variables.kt
+++ unit02/variables.kt
@@ -1,11 +1,13 @@
 fun main(){
-  val myAge = 18 
-  var averageAge = 24 
-  val testNumber = 1 
-  var evenOdd = "testNumber" 
-  var answer = 0
-  println(evenOdd)
-  println(answer+1)
-  println(answer+10)
-  println(answer*10)
+    val myAge = 18
+    var averageAge:Double
+    averageAge = 24.0
+    val testNumber = 6
+    val evenOdd = testNumber
+    var answer: Int
+    answer = 0
+    println(evenOdd)
+    println(answer+1)
+    println(answer+1+10)
+    println(answer+1+10*10)
 }
