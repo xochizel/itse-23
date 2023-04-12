a2f0cfe6a42c4abf35f380b2f063887fc0cc2d0e
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-11 20:08:31 -0500

    Create stringMani.kt

---
+++ unit02/stringMani.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Enter your favorite city")
+    var favoriteCity: String = readln()
+    println(favoriteCity.toUpperCase())
+    println(favoriteCity.toLowerCase())
+    println(favoriteCity.first())
+
+
+}
