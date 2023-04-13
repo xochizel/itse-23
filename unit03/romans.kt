c3f1a91b5a883599581cde225b57da56213f1adc
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-12 20:47:46 -0500

    Update romans.kt

--- unit03/romans.kt
+++ unit03/romans.kt
@@ -1,2 +1,26 @@
-if boom 
-else bam
+fun main(){
+    println("Enter Number 1-10")
+    var userNumb = Integer.valueOf(readLine())
+   if (userNumb==1) {
+        println("l")}
+    else if (userNumb==2)
+        println("ll")
+   else if(userNumb==3){
+       println("lll")}
+   else if(userNumb==4){
+       println("lV")}
+   else if(userNumb==5){
+       println("V")}
+   else if(userNumb==6){
+       println("Vl")}
+   else if(userNumb==7){
+       println("Vll")}
+   else if(userNumb==8){
+       println("Vlll")}
+   else if(userNumb==9){
+       println("lX")}
+   else if(userNumb==10){
+       println("X")}
+   else {
+       println("ERROR") }
+}
