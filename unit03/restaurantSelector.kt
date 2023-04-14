52954cda8f045d584b59681ca4b8564c4218a96a
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-14 18:45:55 -0500

    Update restaurantSelector.kt

--- unit03/restaurantSelector.kt
+++ unit03/restaurantSelector.kt
@@ -1,22 +1,22 @@
 fun main() {
-    val restaurant = mutableListOf("joe", "main", "corner", "mama", "chef")
+    val restaurant = mutableListOf("joe's burger", "main st pizza", "corner cafe", "mama's italian", "chef's kitchen")
     println("is anyone vegetarian")
     var veg = readln()
     when (veg) {
-        "yes" -> { restaurant.remove("joe") }
+        "yes" -> { restaurant.remove("joe's burger") }
     }
     println("is anyone vegan")
     var vegan = readln()
     when (vegan) {
-        "yes" -> { restaurant.remove("joe")
-            restaurant.remove("main")
-            restaurant.remove("mama") }
+        "yes" -> { restaurant.remove("joe's burger")
+            restaurant.remove("main st pizza")
+            restaurant.remove("mama's italian") }
     }
     println("is anyone gluten-free")
     var glute = readln()
     when (glute) {
-        "yes" -> { restaurant.remove("joe")
-        restaurant.remove("mama") }
+        "yes" -> { restaurant.remove("joe's burger")
+        restaurant.remove("mama's italian") }
     }
     println("$restaurant")
 }
