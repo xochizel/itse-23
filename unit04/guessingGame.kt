8bd7aa88641cbe2f9365584f92efc0ca1f5fc464
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-17 01:54:54 -0500

    Create guessingGame.kt

---
+++ unit04/guessingGame.kt
@@ -0,0 +1,19 @@
+import kotlin.random.Random
+
+fun main() {
+    val random = Random.nextInt(10)
+    println("$random")
+
+    do {
+        println("enter number")
+        val userGuess = Integer.valueOf(readln())
+        if (userGuess < random) {
+            println("too low")
+        } else if (userGuess > random) {
+            println("too high")
+        } else if (userGuess==random) {
+            println("correct!")
+        }
+    } while (userGuess<random || userGuess>random)
+
+}
