f83b49aa4d3fa1083d35a7e0ec99e56bda634777
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-17 02:32:16 -0500

    Create barChart.kt

---
+++ unit04/barChart.kt
@@ -0,0 +1,36 @@
+fun main() {
+    println("store 1")
+    val store1 = (Integer.valueOf(readln())/100)
+    println("store 2")
+    val store2 = (Integer.valueOf(readln())/100)
+    println("store 3")
+    val store3 = (Integer.valueOf(readln())/100)
+    println("store 4")
+    val store4 = (Integer.valueOf(readln())/100)
+    println("store 5")
+    val store5 = (Integer.valueOf(readln())/100)
+    println("store 1")
+    for (i in 1..store1) {
+        print("*")
+    }
+    println()
+    println("store 2")
+    for (i in 1..store2) {
+        print("*")
+    }
+    println()
+    println("store 3")
+    for (i in 1..store3) {
+        print("*")
+    }
+    println()
+    println("store 4")
+    for (i in 1..store4) {
+        print("*")
+    }
+    println()
+    println("store 5")
+    for (i in 1..store5) {
+        print("*")
+    }
+}
