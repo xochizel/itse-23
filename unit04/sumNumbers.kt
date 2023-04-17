ac7e8aba8fad3adb44db02b820576bda98052a52
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-16 20:48:35 -0500

    Update sumNumbers.kt

--- unit04/sumNumbers.kt
+++ unit04/sumNumbers.kt
@@ -1,1 +1,19 @@
-bingo bango
+fun main() {
+    var numSum = 0
+    println("Enter a positive non zero integer value")
+    var userInput = Integer.valueOf(readLine())
+    try {
+        if (userInput<51 && userInput >0) {
+            var listInputs = 0..userInput
+            println("Your sum is")
+            print(listInputs.sum())
+        }
+        else {
+            println("Invalid input. Please enter a positive integer.")
+        }
+    }
+    catch(e:Exception) {
+        println("Invalid input. Please enter a positive integer.")
+    }
+
+}
