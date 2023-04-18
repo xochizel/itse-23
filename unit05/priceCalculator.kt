e5e7af31cca15138d894ef2eebc4c788aaf1fb7c
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-17 21:33:40 -0500

    Create priceCalculator.kt

---
+++ unit05/priceCalculator.kt
@@ -0,0 +1,5 @@
+// asks the user to enter item’s wholesale cost and markup percentage. 
+// then display the item’s retail price
+// wholesale cost is 5.00 and markup percentage is 100 percent, then retail price is 10.00.
+// wholesale cost is 5.00 and markup percentage is 50 percent, then retail price is 7.50.
+// have a function named calculateRetail that receives wholesale cost and markup percentage as arguments, and returns retail price 
