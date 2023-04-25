d291700a80ee71d9067d9b75e4def5dda92539de
xochizel <122939425+xochizel@users.noreply.github.com>
2023-04-24 19:47:52 -0500

    Update massWeight.kt

--- unit03/massWeight.kt
+++ unit03/massWeight.kt
@@ -1,7 +1,7 @@
 fun main() {
     println("enter mass of object in kilograms")
     var mass = Integer.valueOf(readLine())
-    val weight = mass * 98
+    val weight = mass * 9.8
     val answer = when {
         weight > 1000 -> "heavy"
         weight < 10 -> "light"
